d52887f877187a0df26b0b59f6f18f940c7d11b7
Radu <radugerm@gmail.com>
2023-04-02 13:35:05 +0200

    modify Test With Rahul

--- cypress/e2e/TesteRandom/TestWithRahul.cy.js
+++ cypress/e2e/TesteRandom/TestWithRahul.cy.js
@@ -6,8 +6,22 @@ describe("Tests with Rahul", () => {
         cy.wait(2000);
         cy.get('.product:visible').should('have.length', 4);  //get only the visible ones
         cy.get('.products').find('.product').should('have.length', 4); //get elements from parent to child
-        //cy.get('.products').find('.product').eq(1).contains("ADD TO CART").click() // chose with index 
-        cy.get(':nth-child(2) > .product-action > button').click() //chose from parent to child
+        //cy.get('.products').find('.product').eq(1).contains("ADD TO CART").click() // chose with index and select the second product what is index 1
+        cy.get(':nth-child(2) > .product-action > button').click() //chose from parent to child and select the second product
+        // down here we iterate through the list to find the specific product
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+
+            const textVeg = $el.find("h4.product-name").text();
+            if (textVeg.includes("Cashews")) {
+
+                cy.wrap($el).find('button').click();
+
+
+            }
+
+
+        })
+
     })
 
 }) 
