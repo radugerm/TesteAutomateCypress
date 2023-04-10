6c2a346f5a832f1e80327d193fbdd724b3b24f45
Radu <radugerm@gmail.com>
2023-04-10 19:11:24 +0200

    Edited

--- cypress/e2e/TesteRandom/TestWithRahul.cy.js
+++ cypress/e2e/TesteRandom/TestWithRahul.cy.js
@@ -1,31 +1,58 @@
 /// <reference types="Cypress" />
-describe("Tests with Rahul", () => {
-    it("Visit site", () => {
-        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-        cy.get('.search-keyword').type("ca");
-        cy.wait(2000);
-        cy.get('.product:visible').should('have.length', 4);  //get only the visible ones
-        cy.get('.products').find('.product').should('have.length', 4); //get elements from parent to child
-        //cy.get('.products').find('.product').eq(1).contains("ADD TO CART").click() // chose with index and select the second product what is index 1
-        cy.get(':nth-child(2) > .product-action > button').click() //chose from parent to child and select the second product
-        // down here we iterate through the list to find the specific product
-        cy.get('.products').find('.product').each(($el, index, $list) => {
 
-            const textVeg = $el.find("h4.product-name").text();
-            if (textVeg.includes("Cashews")) {
+describe('My First Test Suite', function () {
 
-                cy.wrap($el).find('button').click();
+    it('My FirstTest case', function () {
 
 
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        //selenium get hit url in browser, cypress get acts like findElement of selenium
+        cy.get('.product').should('have.length', 5)
+        cy.get('.product:visible').should('have.length', 4)
+        //Parent child chaining
+        cy.get('.products').as('productLocator')
+        cy.get('@productLocator').find('.product').should('have.length', 4)
+        cy.get(':nth-child(3) > .product-action > button').click()
+        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click().then(function () {
+            console.log('sf')
+        })
+
+        cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+
+            const textVeg = $el.find('h4.product-name').text()
+            if (textVeg.includes('Cashews')) {
+                cy.wrap($el.find('button')).click()
             }
+        })
 
-            cy.get('.brand').then(function (logoelement) {
-                cy.log(logoelement.text())
-            })
+        //assert if logo text is correctly displayed
+        cy.get('.brand').should('have.text', 'GREENKART')
+
+        //this is to print in logs
+        cy.get('.brand').then(function (logoelement) {
+            cy.log(logoelement.text())
 
         })
+        //const logo=cy.get('.brand')
+        //cy.log(cy.get('.brand').text())
+        // cy.log(logo.text())
+
+
+
+
+
+
+
+
+
+
+
+        //fixture
 
     })
 
-})
 
+
+})
